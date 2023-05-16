60dfe9a871cfb3af1d49424df5ba94850b8458dd
Edwin Martinez <edwin.martinez11@upr.edu>
2023-05-16 09:34:51 -0400

    Changed to Helloworld buldible

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='helloworld',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/Bolillo07/Jenkins-Experiment.git',
+    author='Edwin Martinez',
+    author_email='edwin.martinez11@upr.edu',
+    description='A simple "Hello, World!" Python application',
+    install_requires=[
+        # List your dependencies here (this app has no dependencies)
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
